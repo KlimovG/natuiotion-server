9a51d329dafbea8937757a2ecb0c9c159bb9ef4f
Georgii Klimov <klimovgg@gmail.com>
2022-10-11 01:04:23 +0200

    create graphql module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './modules/database/database.module';
+import { GraphqlModule } from './modules/graphql/graphql.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, GraphqlModule],
 })
 export class CoreModule {}
