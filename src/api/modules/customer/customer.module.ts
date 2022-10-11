2d9dd85d2ce177c587102798b6559766c8a8009a
Georgii Klimov <klimovgg@gmail.com>
2022-10-11 02:07:33 +0200

    update module for customer

--- src/api/modules/customer/customer.module.ts
+++ src/api/modules/customer/customer.module.ts
@@ -1,4 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { CustomerResolver } from './resolver/customer.resolver';
+import { CustomerService } from './service/customer.service';
+import { RobotsOfCustomersModel as RobotsModel } from '../robot/model/robots-of-customers.model';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerModel } from './model/customer.model';
 
-@Module({})
+@Module({
+  imports: [
+    forwardRef(() => RobotsModel),
+    TypeOrmModule.forFeature([CustomerModel]),
+  ],
+  providers: [CustomerResolver, CustomerService],
+  exports: [CustomerService],
+})
 export class CustomerModule {}
