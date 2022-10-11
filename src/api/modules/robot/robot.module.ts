2525a19e26cf4e6bf6eabf6205cf18669e653e60
Georgii Klimov <klimovgg@gmail.com>
2022-10-11 02:08:29 +0200

    update module for robot

--- src/api/modules/robot/robot.module.ts
+++ src/api/modules/robot/robot.module.ts
@@ -1,4 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { RobotsOfCustomersModel } from './model/robots-of-customers.model';
+import { CustomerModel } from '../customer/model/customer.model';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    forwardRef(() => CustomerModel),
+    TypeOrmModule.forFeature([RobotsOfCustomersModel]),
+  ],
+})
 export class RobotModule {}
