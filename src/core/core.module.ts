fbc95be53732edaa005a8b8c4a904ab72b3ad840
Georgii Klimov <klimovgg@gmail.com>
2022-10-12 22:17:57 +0200

    add auth -> core

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './modules/database/database.module';
 import { GraphqlModule } from './modules/graphql/graphql.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, GraphqlModule],
+  imports: [DatabaseModule, GraphqlModule, AuthModule],
 })
 export class CoreModule {}
