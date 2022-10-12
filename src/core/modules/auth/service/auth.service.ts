eb885b1c2e03beabc1287d83c84233b6dd1a1bee
Georgii Klimov <klimovgg@gmail.com>
2022-10-12 23:26:51 +0200

    update strategy and service

--- src/core/modules/auth/service/auth.service.ts
+++ src/core/modules/auth/service/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../../../../api/modules/user/service/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserModel } from '../../../../api/modules/user/model/user.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByLoginAndPassword(
@@ -16,5 +21,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login({ email, id }: UserModel) {
+    const payload = { username: email, sub: id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
