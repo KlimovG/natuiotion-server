e54bfe42aef4b07407e0c82128d75f98198c2081
Georgii Klimov <klimovgg@gmail.com>
2022-10-13 00:16:01 +0200

    add port from env variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(Number(PORT));
 }
+
 bootstrap();
