ac64d6dd405e4fb8f1bffd1e33ae3c31952cd248
Georgii Klimov <klimovgg@gmail.com>
2022-10-13 00:15:17 +0200

    update core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './modules/database/database.module';
-import { GraphqlModule } from './modules/graphql/graphql.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, GraphqlModule, AuthModule],
+  imports: [AuthModule],
+  exports: [AuthModule],
 })
 export class CoreModule {}
