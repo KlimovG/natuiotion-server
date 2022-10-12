e96b1d5e70fffb6f3110e9e0c74924f82c247b66
Georgii Klimov <klimovgg@gmail.com>
2022-10-13 00:15:41 +0200

    create auth request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+  Inject,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './core/modules/auth/utils/jwt-auth-guard';
+import { AuthService } from './core/modules/auth/service/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(AuthService) private auth: AuthService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.auth.login(req.user);
   }
 }
