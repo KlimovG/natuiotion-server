1256348cd343dcd2b84e60679f4b9ed448417df0
Georgii Klimov <klimovgg@gmail.com>
2022-10-22 22:07:42 +0200

    add weed types model

--- src/api/modules/sessions/sessions.module.ts
+++ src/api/modules/sessions/sessions.module.ts
@@ -5,11 +5,16 @@ import { SessionsModel } from './models/sessions.model';
 import { SessionsResolver } from './resolver/sessions.resolver';
 import { SessionsService } from './service/sessions.service';
 import { VescStatisticModel } from './models/ves-statistic.model';
+import { WeedTypesModel } from '../robot/model/weed-types.model';
 
 @Module({
   imports: [
     forwardRef(() => RobotsModule),
-    TypeOrmModule.forFeature([SessionsModel, VescStatisticModel]),
+    TypeOrmModule.forFeature([
+      SessionsModel,
+      VescStatisticModel,
+      WeedTypesModel,
+    ]),
   ],
   providers: [SessionsResolver, SessionsService],
 })
