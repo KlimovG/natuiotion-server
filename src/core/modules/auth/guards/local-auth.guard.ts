68b89179bab4ddfe4940d54f59845855afc7dc4e
Georgii Klimov <klimovgg@gmail.com>
2023-02-23 01:16:42 +0100

    remove console.logs

--- src/core/modules/auth/guards/local-auth.guard.ts
+++ src/core/modules/auth/guards/local-auth.guard.ts
@@ -5,7 +5,6 @@ export class LocalAuthGuard extends AuthGuard('local') {
   async canActivate(context: ExecutionContext) {
     const result = (await super.canActivate(context)) as boolean;
     const request = context.switchToHttp().getRequest();
-    console.log('hui');
     if (request.user) {
       // Set tokens to request object
       request.tokens = request.user.tokens;
