2ceb86cc6448d25a7a9488063b900cbee6e17025
Georgii Klimov <klimovgg@gmail.com>
2023-03-05 21:01:44 +0100

    add functionality to get 10 more sessions

--- src/api/modules/sessions/service/sessions.service.ts
+++ src/api/modules/sessions/service/sessions.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SessionsModel } from '../models/sessions.model';
-import { Repository } from 'typeorm';
+import { LessThan, Repository } from 'typeorm';
 import { BaseService } from '../../../../utils/base-models';
 
 @Injectable()
 export class SessionsService implements BaseService<SessionsModel> {
+  private readonly logger = new Logger('Session');
+
   constructor(
     @InjectRepository(SessionsModel)
     private sessionRepo: Repository<SessionsModel>,
@@ -15,7 +17,7 @@ export class SessionsService implements BaseService<SessionsModel> {
     return this.sessionRepo.find();
   }
 
-  findAllByName(robotSerialNumber: string): Promise<SessionsModel[]> {
+  async findAllByName(robotSerialNumber: string): Promise<SessionsModel[]> {
     return this.sessionRepo.find({
       where: {
         robotNumber: robotSerialNumber,
@@ -32,5 +34,32 @@ export class SessionsService implements BaseService<SessionsModel> {
 
   findOne(id: number): Promise<SessionsModel> {
     return this.sessionRepo.findOneBy({ id });
+  }
+
+  async findAllByNameStartFromSessionWithId(
+    robotSerialNumber: string,
+    startSessionId: number,
+  ): Promise<SessionsModel[]> {
+    const startSession = await this.findOne(startSessionId);
+    if (!startSession) {
+      throw new Error('Start session not found');
+    }
+    this.logger.log(
+      `Find 10 more sessions from ${startSession.startTime.toDateString()} for robot ${robotSerialNumber}`,
+    );
+
+    return this.sessionRepo.find({
+      where: {
+        robotNumber: robotSerialNumber,
+        startTime: LessThan(startSession.startTime),
+      },
+      relations: {
+        extractedWeeds: true,
+      },
+      order: {
+        startTime: 'DESC',
+      },
+      take: 10,
+    });
   }
 }
