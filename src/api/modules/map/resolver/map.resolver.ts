e66503fefdd66fe0ca93a5f623c243d18a7e0333
Georgii Klimov <klimovgg@gmail.com>
2023-03-16 03:46:40 +0100

    clean file

--- src/api/modules/map/resolver/map.resolver.ts
+++ src/api/modules/map/resolver/map.resolver.ts
@@ -9,10 +9,8 @@ import {
 import { Inject, UseGuards } from '@nestjs/common';
 import { MapService } from '../service/map.service';
 import { FieldModel } from '../models/field.model';
-import { ExtractedDto } from '../dto/extracted.dto';
 import { AccessTokenGuard } from '../../../../core/modules/auth/guards/access-token.guard';
 import { MapDataDto } from '../dto/map-data.dto';
-import { PathDto } from '../dto/path.dto';
 
 @Resolver(FieldModel)
 export class MapResolver {
@@ -20,20 +18,9 @@ export class MapResolver {
     @Inject(MapService)
     private service: MapService,
   ) {}
-  @UseGuards(AccessTokenGuard)
-  @Query(() => FieldModel)
-  async getField(@Args('id') id: number): Promise<FieldModel> {
-    return await this.service.getField(id);
-  }
 
   @UseGuards(AccessTokenGuard)
-  @Query(() => [ExtractedDto], { nullable: true })
-  getExtractedPoints(@Args('sessionId') sessionId: number) {
-    return this.service.getExtractedPoints(sessionId);
-  }
-
-  @UseGuards(AccessTokenGuard)
-  @Query(() => [MapDataDto], { nullable: true })
+  @Query(() => MapDataDto, { nullable: true })
   async getMapData(@Args('sessionId') sessionId: number): Promise<MapDataDto> {
     const field = await this.service.getField(sessionId);
     const path = await this.service.getPath(sessionId);
@@ -44,12 +31,6 @@ export class MapResolver {
       path,
       extracted,
     };
-  }
-
-  @UseGuards(AccessTokenGuard)
-  @Query(() => PathDto, { nullable: true })
-  async getPath(@Args('sessionId') sessionId: number): Promise<PathDto> {
-    return await this.service.getPath(sessionId);
   }
 
   @UseGuards(AccessTokenGuard)
